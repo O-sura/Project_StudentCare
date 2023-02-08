b81e0f7a06419ebc8bb72754be423adbd89fe881
ChamyaShamindi <2020cs175@stu.ucsc.cmb.ac.lk>
2023-02-08 22:51:15 +0530

    message modify

--- app/views/facility_provider/message.php
+++ app/views/facility_provider/message.php
@@ -32,14 +32,14 @@
                         </div>
                     </div>
                     <div class="chat_profiles">
-                        <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="">
+                        <img src="https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="">
                         <div class="short">
                             <p>Damien</p>
                             <span>what can i do for you</span>
                         </div>
                     </div>
                     <div class="chat_profiles">
-                        <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="">
+                        <img src="https://images.unsplash.com/photo-1654110455429-cf322b40a906?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
                         <div class="short">
                             <p>ScottTS</p>
                             <span>what can i do for you</span>
