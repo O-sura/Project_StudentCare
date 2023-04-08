698c7364827a758b290c61891a4e4352361982a0
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-04-08 17:16:18 +0530

    prof pic

--- app/controllers/Student.php
+++ app/controllers/Student.php
@@ -1,30 +1,158 @@
 <?php
-    Session::init();
-    class Student extends Controller{
-        private $studentModel;
-        public function __construct(){
-            Middleware::authorizeUser(Session::get('userrole'), 'student');
-            $this->studentModel = $this->loadmodel('Student_model');
-        }
-        
-        public function index(){
-
-        }
-
-        public function home(){
-            $usr =   Session::get('username');
-            $new_requests_count = $this->studentModel->getNewRequestsCount(Session::get('userID'));
-            $new_appointments_count = $this->studentModel->getNewAppointmentsCount(Session::get('userID'));
-            $total_count = $new_requests_count + $new_appointments_count;
-            $data = ['username' => $usr,
-                    'new_requests_count' => $total_count];
-            $this->loadview('student_dashboard/index',$data);
-        }
-
-        public function profile(){
-            $id =   Session::get('userID');
-            $data = ['userDetails' => $this->studentModel->getProfile($id)];
-            $this->loadview('student_dashboard/profile',$data);
-        }
-
+Session::init();
+class Student extends Controller
+{
+    private $studentModel;
+    public function __construct()
+    {
+        Middleware::authorizeUser(Session::get('userrole'), 'student');
+        $this->studentModel = $this->loadmodel('Student_model');
     }
+
+    public function index()
+    {
+    }
+
+    public function home()
+    {
+        $usr =   Session::get('username');
+        $new_requests_count = $this->studentModel->getNewRequestsCount(Session::get('userID'));
+        $new_appointments_count = $this->studentModel->getNewAppointmentsCount(Session::get('userID'));
+        $total_count = $new_requests_count + $new_appointments_count;
+        $data = [
+            'username' => $usr,
+            'new_requests_count' => $total_count,
+            'userDetails' => $this->studentModel->getProfile(Session::get('userID'))
+        ];
+        $this->loadview('student_dashboard/index', $data);
+    }
+
+    public function profile()
+    {
+
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $row = $this->studentModel->getProfile(Session::get('userID'));
+            $filename = $_FILES["file"]["name"];
+            $tempname = $_FILES["file"]["tmp_name"];
+            $folder =  PUBLICPATH . "img/student/" . $filename;
+
+
+
+            if (move_uploaded_file($tempname, $folder)) {
+            } else if (empty($filename) && empty($tempname)) {
+                $filename = $row->profile_img;
+                $folder = PUBLICPATH . "img/student/" . $filename;
+                $tempname = tempnam(sys_get_temp_dir(), 'image_');
+                copy($folder, $tempname);
+            }
+
+            //Check and validate the data
+            //Set errors if something is wrong
+            $name = $_POST['name'];
+            $username = $_POST['uname'];
+            $address = $_POST['address'];
+            $contact = $_POST['phone'];
+
+
+
+
+            $data = [
+                'profile_img' => $filename,
+                'name' => $name,
+                'username' => $username,
+                'contact' => $contact,
+                'address' => $address,
+                'profile' => $row->profile_img,
+                'name_err' => '',
+                'username_err' => '',
+                'contact_err' => '',
+                'address_err' => ''
+            ];
+
+
+            //Check whether all the fields are filled properly
+            if (empty($data['username'])) {
+                //echo("Must fill all the fields in the form!");
+                $data['username_err'] = "*Username field is Required";
+
+                // print_r ($data);
+                // exit;
+            }
+
+            if (empty($data['name'])) {
+                $data['name_err'] =  "*Name field is Required";
+                // print_r ($data);
+                // exit;
+            }
+
+            if (empty($data['address'])) {
+                $data['address_err'] = "*Address field is Required";
+                // print_r ($data);
+                // exit;
+            }
+
+            if (empty($data['contact'])) {
+                $data['contact_err'] = "*Contact field is Required";
+                // print_r ($data);
+                // exit;
+            }
+
+            //Check whether an account already exists with the provided username
+            if ($this->studentModel->findUserByUsername($username)) {
+                //echo("This Username is already taken");
+                if ($username == Session::get('username')) {
+                    $data['username_err'] = "";
+                } else {
+                    $data['username_err'] = "*This Username is already taken";
+                }
+
+                //die();
+            }
+
+            //Check the mobile number
+            if (strlen($contact) != 10) {
+                //echo 'Invalid Contact Number';
+                $data['contact_err'] = "*Invalid Contact Number";
+                //die();
+            }
+
+            // print_r ($data);
+            //     exit;
+            //Make sure there are no error flags are set
+            if (empty($data['username_err']) && empty($data['name_err'])  && empty($data['contact_err']) && empty($data['address_err'])) {
+
+                // print_r ($data);
+                //  exit;
+
+                $res = $this->studentModel->updateProfileDetails($data, Session::get('userID'));
+
+                if ($res) {
+                    FlashMessage::flash('update_profile_flash', "Successfully Updated Your Profile Details!", "success");
+                    Student::home();
+                } else {
+                    //Error Notification
+                    echo 'Error: Something went wrong in adding post to the databse';
+                    Student::home();
+                    die();
+                }
+            } else {
+                $id =   Session::get('userID');
+                $row = $this->studentModel->getProfile($id);
+                $data['userDetails'] = $this->studentModel->getProfile($id);
+                $this->loadview('student_dashboard/profile', $data);
+            }
+        } else {
+            $id =   Session::get('userID');
+            $row = $this->studentModel->getProfile($id);
+            $data = [
+                'userDetails' => $this->studentModel->getProfile($id),
+                'name_err' => '',
+                'username_err' => '',
+                'address_err' => '',
+                'contact_err' => ''
+            ];
+            $this->loadview('student_dashboard/profile', $data);
+        }
+    }
+}
--- app/views/student_dashboard/index.php
+++ app/views/student_dashboard/index.php
@@ -70,7 +70,14 @@
         <div class="profile_content">
             <div class="profile">
                 <div class="profile_details">
-                    <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80">
+                    <?php
+                    if ($data["userDetails"]->profile_img != NULL) {
+                        $image = $data["userDetails"]->profile_img;
+                    } else {
+                        $image = "avatar.jpg";
+                    }
+                    ?>
+                    <img src="<?php echo URLROOT . "/public/img/student/" . $image; ?>">
                     <div class="name">
                         <?php echo $data['username'] ?>
                     </div>
@@ -88,7 +95,7 @@
                 <div class="user-details">
                     <h2>Profile</h2>
                     <div class="prof-img">
-                        <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="" id="image2">
+                        <img src="<?php echo URLROOT . "/public/img/student/" . $image; ?>" alt="" id="image2">
                     </div>
                     <div class="prof-details">
                         <h3><?php echo $data['username'] ?></h3><br><br>
